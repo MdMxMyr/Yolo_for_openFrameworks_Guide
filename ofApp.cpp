02c804ac712ba42e85abde967a29849adc9a318d
MdMxMyr <maxmeyer@live.nl>
2020-01-28 00:30:43 +0100

    Starter File
    Once you've completed this guide: https://docs.google.com/document/d/1cFoVgV5OFLqVHUeebkF7n4Z5ryT0g5bHWd7K55ltc7A/edit?usp=sharing, use the code-commit in this repo as a starting point

---
+++ ofApp.cpp
@@ -0,0 +1,98 @@
+#include "ofApp.h"
+
+std::vector<std::string> ofApp::objects_from_file(std::string const filename) {
+	std::ifstream file(filename);
+	std::vector<std::string> file_lines;
+	if (!file.is_open()) return file_lines;
+	for (std::string line; getline(file, line);) file_lines.push_back(line);
+	std::cout << "object names loaded \n";
+	return file_lines;
+}
+
+void ofApp::show_console_result(std::vector<bbox_t> const result_vec, std::vector<std::string> const obj_names, int frame_id = -1) {
+	if (frame_id >= 0) std::cout << " Frame: " << frame_id << std::endl;
+	for (auto &i : result_vec) {
+		if (obj_names.size() > i.obj_id) std::cout << obj_names[i.obj_id] << " - ";
+		std::cout << "obj_id = " << i.obj_id << ",  x = " << i.x << ", y = " << i.y
+			<< ", w = " << i.w << ", h = " << i.h
+			<< std::setprecision(3) << ", prob = " << i.prob << std::endl;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	cap.open(0);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	cap >> cur_frame;
+	frame_id += 1;
+
+	result_vec = detector.detect(cur_frame);
+	result_vec = detector.tracking_id(result_vec);	
+	cout << result_vec.size() << "\n";
+
+	show_console_result(result_vec, obj_names, frame_id);
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ ofApp.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include "ofMain.h"
+
+#include <chrono>
+#include <iostream>
+#include <fstream>
+
+#include <iomanip> 
+#include <string>
+#include <vector>
+#include <queue>
+#include <thread>
+#include <atomic>
+#include <mutex>              // std::mutex, std::unique_lock
+#include <condition_variable> // std::condition_variable
+
+#ifdef _WIN32
+#define OPENCV
+#define GPU
+#endif
+
+#include "D:/YoloOpenFrameworks/darknet-master/include/yolo_v2_class.hpp"
+
+#ifdef OPENCV
+#include <opencv2/opencv.hpp>            // C++
+#include "opencv2/core/version.hpp"
+#ifndef CV_VERSION_EPOCH
+#include "opencv2/videoio/videoio.hpp"
+#define OPENCV_VERSION CVAUX_STR(CV_VERSION_MAJOR)"" CVAUX_STR(CV_VERSION_MINOR)"" CVAUX_STR(CV_VERSION_REVISION)
+#pragma comment(lib, "opencv_world" OPENCV_VERSION ".lib")
+//#include "C:\opencv\build\x64\vc14\lib\opencv_world340.dll"
+#ifdef TRACK_OPTFLOW
+#pragma comment(lib, "opencv_cudaoptflow" OPENCV_VERSION ".lib")
+#pragma comment(lib, "opencv_cudaimgproc" OPENCV_VERSION ".lib")
+#pragma comment(lib, "opencv_core" OPENCV_VERSION ".lib")
+#pragma comment(lib, "opencv_imgproc" OPENCV_VERSION ".lib")
+#pragma comment(lib, "opencv_highgui" OPENCV_VERSION ".lib")
+#endif    // TRACK_OPTFLOW
+#else
+#define OPENCV_VERSION CVAUX_STR(CV_VERSION_EPOCH)""CVAUX_STR(CV_VERSION_MAJOR)""CVAUX_STR(CV_VERSION_MINOR)
+#pragma comment(lib, "opencv_core" OPENCV_VERSION ".lib")
+#pragma comment(lib, "opencv_imgproc" OPENCV_VERSION ".lib")
+#pragma comment(lib, "opencv_highgui" OPENCV_VERSION ".lib")
+#endif    // CV_VERSION_EPOCH
+
+#endif
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		std::string names_file = "./bin/data/coco.names";
+		std::string cfg_path = "./bin/cfg/yolov3.cfg";
+		std::string weight_path = "./bin/yolov3.weights";
+
+		std::vector<std::string> objects_from_file(std::string const filename);
+		void show_console_result(std::vector<bbox_t> const result_vec, std::vector<std::string> const obj_names, int frame_id);
+
+		std::vector<std::string> obj_names = objects_from_file(this->names_file);
+
+		Detector detector = Detector(this->cfg_path, this->weight_path);
+
+		cv::VideoCapture cap;
+		cv::Mat cur_frame;
+		std::vector<bbox_t> result_vec;
+
+		int frame_id = 0;
+
+};
